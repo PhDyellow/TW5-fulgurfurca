8943cab8780f6b58e4e260dfc749f163cbf43a57
phildyer <phildyeryellow@gmail.com>
2015-11-04 14:12:24 +1000

    update demo site

---
+++ build.bat
@@ -0,0 +1,7 @@
+REM build index.html
+
+set PATH_TO_NODE=%~dp0..\..\..\..\Software\win\nodejs\
+
+copy /Y demowiki\tiddlywiki.info.nodejs demowiki\tiddlywiki.info
+
+%PATH_TO_NODE%\node.exe %PATH_TO_NODE%\node_modules\tiddlywiki\tiddlywiki.js demowiki --verbose --output .\ --build
